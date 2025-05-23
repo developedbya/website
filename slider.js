b1547dcf224ca5853ca0662ee7efecf4e24ca3c6
developedbya <developedbyaudrey@gmail.com>
2025-05-23 15:10:03 -0400

    Update slider.js

--- slider.js
+++ slider.js
@@ -2,7 +2,7 @@ var slider = tns({
     container: '.my-slider',
     items: 1,
     slideBy: 'page',
-    "speed" : 700,
+    "speed" : 500,
     autoplay: true,
     controls: false,
     nav: false,
